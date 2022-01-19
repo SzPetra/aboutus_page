f6159cbac58ab952e341c7deb4791e7c49be6b84
SzPetra <petra.szilagyi27@gmail.com>
2022-01-19 09:53:36 +0100

    everybody owns a page

--- pages.py
+++ pages.py
@@ -6,9 +6,37 @@ app=Flask(__name__)
 def aron_page():
 	return render_template("aaron.html")
 
+@app.route("/balazs")
+def balazs_page():
+	return render_template("balazs.html")
+
+@app.route("/imi")
+def imi_page():
+	return render_template("imi.html")
+
+@app.route("/jeno")
+def jeno_page():
+	return render_template("jeno.html")
+
+@app.route("/lorand")
+def lorand_page():
+	return render_template("lorand.html")
+
+@app.route("/miki")
+def miki_page():
+	return render_template("miki.html")
+
+@app.route("/petra")
+def petra_page():
+	return render_template("petra.html")
+
+@app.route("/rebi")
+def rebi_page():
+	return render_template("rebi.html")
+
 @app.route("/")
 def main_page():
 	return render_template("aboutus.html")
 	
 if __name__=="__main__":
-	app.run(debug=True, port=5000)
+	app.run(debug=True, port=8000)
